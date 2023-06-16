02525eff25cc142fbc9b19076e63f6d78268844c
jliliflora <jliliflora@wonderpeople.co.kr>
2023-06-16 18:54:15 +0900

    AnimatePresence

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { motion, useMotionValue, useTransform } from "framer-motion";
-import { useRef } from "react";
+import { useState, useRef } from "react";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
@@ -9,7 +9,7 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
-  background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
+  // background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 
 // 애니메이트된 스타일 컴포넌트를 사용하는 법!
@@ -22,24 +22,12 @@ const Box = styled(motion.div)`
 `;
 
 function App() {
-  const x = useMotionValue(0); // 좌표가 필요할때
-  const potato = useTransform(x, [-800, 0, 800], [2, 1, 0.1]); // 800 -> 2 값으로 전달 받고싶을때
-  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
-  const gradient = useTransform(
-    x,
-    [-800, 800],
-    [
-      "linear-gradient(135deg, rgb(0, 202, 238), rgb(0, 56, 238))",
-      "linear-gradient(135deg, rgb(238, 202, 0), rgb(0, 238, 32))",
-    ]
-  );
+  const [showing, setShowing] = useState(false);
   return (
-    <Wrapper style={{ background: gradient }}>
-      <button onClick={() => x.set(200)}>clickme</button>
-      <Box style={{ x, rotateZ }} drag="x" dragSnapToOrigin />
+    <Wrapper>
+      <button>Click</button>
+      {showing ? <Box /> : null}
     </Wrapper>
   );
-  // dragSnampToOrigin : 원점으로 알아서 돌아오는 기능
-  // <Box style={{ x, scale: potato }}
 }
 export default App;
