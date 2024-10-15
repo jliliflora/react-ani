ece136523924a9c073df531506fcb8104dd326f3
jliliflora <syon704@gmail.com>
2024-10-15 20:57:16 +0900

    scroll animation 완성

---
+++ src/Components/Section3.tsx
@@ -0,0 +1,31 @@
+import { motion, MotionValue, useTransform } from "framer-motion";
+import styled from "styled-components";
+
+const Sec = styled(motion.div)`
+  position: sticky;
+  top: 0px;
+  height: 100vh;
+  background-color: rgb(41, 77, 194);
+  font-size: 3.5vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  padding-bottom: 10vh;
+`;
+interface SectionProps {
+  scrollYProgress: MotionValue<number>; // scrollYProgress의 타입을 정의
+}
+
+function Section1({ scrollYProgress }: SectionProps) {
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [0, -5]);
+  return (
+    <Sec>
+      <motion.p style={{ scale }}>Scroll Perspective</motion.p>
+    </Sec>
+  );
+}
+
+export default Section1;
--- src/ScrollPage.tsx
+++ src/ScrollPage.tsx
@@ -1,83 +1,15 @@
-import { motion, MotionValue, useScroll, useTransform } from "framer-motion";
+import { useScroll } from "framer-motion";
 import Lenis from "lenis";
 import { useEffect, useRef } from "react";
 import styled from "styled-components";
+import Section1 from "./Components/Section1";
+import Section2 from "./Components/Section2";
+import Section3 from "./Components/Section3";
 
 const Main = styled.div`
   position: relative;
   height: 300vh;
 `;
-const Sec001 = styled(motion.div)`
-  position: sticky;
-  top: 0px;
-  height: 100vh;
-  background-color: rgb(212, 189, 17);
-  font-size: 3.5vw;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  color: #fff;
-  padding-bottom: 10vh;
-`;
-const Sec002 = styled(motion.div)`
-  position: sticky;
-  top: 0px;
-  height: 100vh;
-  background-color: rgb(64, 194, 41);
-  font-size: 3.5vw;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  color: #fff;
-  padding-bottom: 10vh;
-`;
-const Sec003 = styled(motion.div)`
-  position: sticky;
-  top: 0px;
-  height: 100vh;
-  background-color: rgb(41, 77, 194);
-  font-size: 3.5vw;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  color: #fff;
-  padding-bottom: 10vh;
-`;
-
-interface SectionProps {
-  scrollYProgress: MotionValue<number>; // scrollYProgress의 타입을 정의
-}
-
-const Section1 = ({ scrollYProgress }: SectionProps) => {
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
-  const rotate = useTransform(scrollYProgress, [0, 1], [0, -5]);
-  return (
-    <Sec001>
-      <motion.p style={{ scale }}>Scroll Perspective</motion.p>
-    </Sec001>
-  );
-};
-const Section2 = ({ scrollYProgress }: SectionProps) => {
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
-  const rotate = useTransform(scrollYProgress, [0, 1], [0, -5]);
-  return (
-    <Sec002>
-      <motion.p style={{ scale }}>Scroll Perspective</motion.p>
-    </Sec002>
-  );
-};
-const Section3 = ({ scrollYProgress }: SectionProps) => {
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
-  const rotate = useTransform(scrollYProgress, [0, 1], [0, -5]);
-  return (
-    <Sec003>
-      <motion.p style={{ scale }}>Scroll Perspective</motion.p>
-    </Sec003>
-  );
-};
 
 function ScrollPage() {
   const container = useRef<HTMLDivElement>(null); // HTMLDivElement 타입으로 설정
